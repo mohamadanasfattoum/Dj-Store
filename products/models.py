b15e7b484b17b8979c5a60464e7bbf4d3174a042
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-28 15:27:57 +0200

    add brand in class product

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
 
 
 FLAG_CHOTCES = (
@@ -15,14 +16,19 @@ class Product(models.Model):
     image = models.ImageField(_('Image'), upload_to= 'product')
     price = models.FloatField(_('Price'))
     flag = models.CharField(_('Flag'),max_length=10, choices=FLAG_CHOTCES)
+    brand = models.ForeignKey('Brand',verbose_name=_('Brand'), related_name='product_brand', on_delete=models.SET_NULL, null=True, blank=True )
     
-
 
 
 class ProductImage(models.Model):
     product =models.ForeignKey(Product, on_delete=models.CASCADE , related_name='product_image')
     image = models.ImageField(_("Image"), upload_to='product_images')
 
+
+class Brand(models.Model):
+    name = models.CharField(_('Name'), max_length=120)
+    image = models.ImageField(_('Image'), upload_to= 'brands')
+    slug = models.SlugField(null=True, blank=True)
 
 class Review(models.Model):
     user = models.ForeignKey(User, related_name= 'review_user',on_delete=models.SET_NULL, null=True, blank=True)#If delete User, not delete the comment
