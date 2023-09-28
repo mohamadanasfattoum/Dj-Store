5f9101aa319913ab9639e52fed09eb546bc27c52
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-28 19:17:44 +0200

    add ProductList in views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView , DeleteView
+from .models import Product
 
-# Create your views here.
+
+class ProductList(ListView):
+    model = Product
+
+
