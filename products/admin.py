357573c0f1c8b081cbeef83663a2275ca883770d
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-28 16:35:33 +0200

    add Editor summernote in admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Product, ProductImage, Brand, Review
 
-admin.site.register(Product)
+
+
+class ProductAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+
+admin.site.register(Product,ProductAdmin)
 admin.site.register(ProductImage)
 admin.site.register(Brand)
 admin.site.register(Review)
