184e9d3e2c98c051a78bd76869b0b5c985ed6c87
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-28 20:03:25 +0200

    add detailview

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DeleteView
+from django.views.generic import ListView , DetailView
 from .models import Product
 
 
@@ -7,5 +7,5 @@ class ProductList(ListView):
     model = Product
 
 
-class ProductDetail(DeleteView):
+class ProductDetail(DetailView):
     model = Product
