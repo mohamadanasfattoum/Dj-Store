9bb00f7f5c97df3275892cab008a337b65b3f898
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-28 19:45:36 +0200

    add push

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,7 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/', include('products.urls')),
+    path('products/', include('products.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
 
