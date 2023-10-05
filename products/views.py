77fda88d9394d906cc18f7096e7dc39b1ecd90b5
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-05 23:49:02 +0200

    add  brand list detail . views

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
-from .models import Product
+from .models import Product , Brand
 
 
 class ProductList(ListView):
@@ -9,3 +9,11 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
     model = Product
+
+
+class BrandList(ListView):
+    model = Brand
+
+
+class BrandDetail(DetailView):
+    model = Brand
