eb69b772d5d9c19d3e9e3eebcc4b220bf366446c
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-06 00:37:28 +0200

    add queryset in  branddetail

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db import models
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
 from .models import Product , Brand
@@ -15,5 +17,11 @@ class BrandList(ListView):
     model = Brand
 
 
-class BrandDetail(DetailView):
-    model = Brand
+class BrandDetail(ListView):
+    model = Product
+    template_name = 'brand_detail.html'
+    def get_queryset(self):
+        queryset = super(BrandDetail, self).get_queryset()
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = queryset.filter(brand=brand)
+        return queryset
