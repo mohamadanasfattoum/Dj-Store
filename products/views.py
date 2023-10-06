e2503d42528e8234eb0b4ab0bfb3a99e713bcd23
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-07 00:17:56 +0200

    push

--- products/views.py
+++ products/views.py
@@ -13,7 +13,7 @@ class ProductDetail(DetailView):
     model = Product
     def get_context_data(self, **kwargs: Any):
         context = super().get_context_data(**kwargs)
-        context['product_image'] = ProductImage.objects.filter(product= self.get_object())
+        context['product_images'] = ProductImage.objects.filter(product= self.get_object())
         context['reviews'] = Review.objects.filter(product= self.get_object())
         return context
 
