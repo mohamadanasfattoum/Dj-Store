b0fed394ada3b855c71f28d2489d4cfd371faac3
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-08 22:40:27 +0200

    create settings app

---
+++ settings/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to='logo/')
+    call_us = models.CharField(max_length=20)
+    email_us = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=300)
+    emails = models.TextField(max_length=300)
+    phones = models.TextField(max_length=300)
+    address = models.TextField(max_length=300)
+    fb_link = models.URLField(null=True, blank=True)
+    tw_link = models.URLField(null=True, blank=True)
+    yt_link = models.URLField(null=True, blank=True)
+    app_description = models.CharField( max_length=300, null=True, blank=True)
+    androud_app = models.URLField(null=True, blank=True)
+    iphone_app = models.URLField(null=True, blank=True)
+
+
+
+    def __str__(self) -> str:
+        return self.name
