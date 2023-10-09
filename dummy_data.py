d6aaf10b814fcf76f19ee24349657ec5f69f2817
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-09 22:05:26 +0200

    add def add_brands dummy_data.py

---
+++ dummy_data.py
@@ -0,0 +1,34 @@
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+
+
+from products.models import Product, Brand, Review
+import random 
+# search to python Faker
+from faker import Faker
+
+
+
+def add_brands(n):
+    fake = Faker()
+    images = ['1.jpeg','2.jpeg','3.jpeg','4.jpeg','5.jpeg','6.jpeg','7.jpeg','8.jpeg','9.jpeg','10.jpeg']
+    for x in range (n):
+        Brand.objects.create(
+            name = fake.name(),
+            image = f"brands/{images[random.randint(0,9)]}"
+        )
+    print(f"{n} brands was created successfully" )
+
+def add_products(n):
+    pass
+
+
+
+
+
+def add_reviews(n):
+    pass
+
+
+add_products(2)
